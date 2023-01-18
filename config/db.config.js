cf8bead1053468c6ca70dd57d90fe6a12d687c65
bjpogs <bjpogs26@gmail.com>
2023-01-18 11:57:52 +0800

    sana

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const con = mysql.createConnection({
     host: process.env.API_HOST,
-    user: process.env.API_USER,
+    user: 'root',
     password: process.env.API_PASSWORD,
     database: process.env.API_DATABASE,
     multipleStatements: true
